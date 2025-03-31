46d00355849d6383026ca5a318deef8e6b2cf0e9
kunalaswar <kunalaswar2004@gmail.com>
2025-03-31 08:52:12 +0530

    Django

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
 # print("Hello Python")
 from sys import argv 
 print(type(argv))
+print("Number of command line args",len(argv))
+print("the List of command line args",argv)
+for val in argv:
+    print(val)
+    
